37860bc775f648fc9b003415e8d9a932ef4906f2
paris-rahman <154307438+paris-rahman@users.noreply.github.com>
2023-12-21 06:29:02 +0000

    Added the initial page with bootstrp

--- app.py
+++ app.py
@@ -1,11 +1,38 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,jsonify
 
 app = Flask(__name__)
+
+JOBS = [
+  {
+    "id" : 1,
+    "title" : "Data Engineer",
+    "location" :"Chennai, India",
+    "salary" : "10,00,000 LPA"
+  },
+  {
+    "id" : 2,
+    "title" : "Python Backend Developer",
+    "location" :"Bangalore,India",
+    "salary" : "15,00,000 LPA"
+  },
+  {
+    "id" : 3,
+    "title" : "Java Backend Developer",
+    "location" :"Mumbai,India",
+    "salary" : "20,00,000 LPA"
+  }
+
+  
+]
 
 
 @app.route("/")
 def hello_world():
-  return render_template("home.html")
+  return render_template("home.html",jobs = JOBS)
+
+@app.route("/api/jobs")
+def display_jobs():
+  return jsonify(JOBS)
 
 
 if __name__ == "__main__":
